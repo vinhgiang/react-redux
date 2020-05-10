24c57967eed6e2ea1eb32be361c072c3362544ef
Kevin <giangcamvinh@gmail.com>
2020-05-10 13:57:16 -0700

    move basic redux to basic folder

--- basic/index.js
+++ basic/index.js
@@ -6,9 +6,11 @@ const combineReducers = redux.combineReducers;
 const applyMiddleware = redux.applyMiddleware;
 const logger = reduxLogger.createLogger();
 
+// Action types. Simply a list of constant
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICE_CREAM = 'BUY_ICE_CREAM';
 
+// Action creator. It will simply return an action which is an object
 const buyCake = () => {
     return {
         type: BUY_CAKE,
@@ -22,6 +24,7 @@ const buyIceCream = () => {
     };
 }
 
+// initial state of applycation
 const initialCakeState = {
     numOfCakes: 10
 };
@@ -30,6 +33,7 @@ const initialIceCreamState = {
     numOfIceCream: 20
 }
 
+// A reducer will transmit state based on type of action
 const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
@@ -50,16 +54,25 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
     }
 };
 
+// combine all reducer together
 const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream: iceCreamReducer
 })
+
+// initial state manager (state store)
 const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial state', store.getState());
+
+// subscribe the store (begin listening)
 const unsubscribe = store.subscribe(() => {});
+
+// trigger actions
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
+
+// unsubscribe the store
 unsubscribe();
